f2296e87d57fb527f1ab412c174a1942c8a2d143
kohzih <kohzih@gmail.com>
2024-05-18 10:32:33 +0900

    feat: BaseButtonの作成

---
+++ CalculatorApp/Models/BaseButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp.Models
+{
+    internal class BaseButton : Button
+    {
+        public BaseButton()
+        {
+            this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Size = new System.Drawing.Size(50, 50);
+            this.Margin = new Padding(5);
+        }
+    }
+}
