eba86209bbb6d54ce014410a6433a0a49db5893b
kohzih <kohzih@gmail.com>
2024-05-18 10:20:11 +0900

    feat: MainFormの作成

--- CalculatorApp/Views/MainForm.Designer.cs
+++ CalculatorApp/Views/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalculatorApp
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,34 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // MainForm
             // 
-            this.button1.Location = new System.Drawing.Point(147, 168);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 100);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(480, 300);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
