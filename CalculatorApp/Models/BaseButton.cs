bb3d21ecd7221444739df16f2c285fd74d332e2a
kohzih <kohzih@gmail.com>
2024-05-18 10:44:23 +0900

    feat: DerivedButtonの作成

--- CalculatorApp/Models/BaseButton.cs
+++ CalculatorApp/Models/BaseButton.cs
@@ -10,26 +10,11 @@ namespace CalculatorApp.Models
 {
     internal class BaseButton : Button
     {
-        private string _displayText;
-
         public BaseButton()
         {
             this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Size = new System.Drawing.Size(50, 50);
             this.Margin = new Padding(5);
-        }
-
-        [Category("Custom Properties")]
-        [Description("表示文字")]
-        public string DisplayText
-        {
-            get { return _displayText; }
-            set
-            {
-                _displayText = value;
-                this.Text = value;
-                Invalidate();  // ボタンの再描画
-            }
         }
     }
 }
---
+++ CalculatorApp/Models/DerivedButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    internal class DerivedButton : BaseButton
+    {
+        private string _displayText;
+
+        [Category("Custom Properties")]
+        [Description("表示文字")]
+        public string DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                _displayText = value;
+                this.Text = value;
+                Invalidate();  // ボタンの再描画
+            }
+        }
+
+        public DerivedButton()
+        {
+            this.Size = new Size(60, 60);
+        }
+    }
+}
