2ba23839d60f3ed03662f50561894ae5c53a49fa
kohzih <kohzih@gmail.com>
2024-05-18 10:37:24 +0900

    feat: 表示文字プロパティの作成

--- CalculatorApp/Models/BaseButton.cs
+++ CalculatorApp/Models/BaseButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,26 @@ namespace CalculatorApp.Models
 {
     internal class BaseButton : Button
     {
+        private string _displayText;
+
         public BaseButton()
         {
             this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Size = new System.Drawing.Size(50, 50);
             this.Margin = new Padding(5);
+        }
+
+        [Category("Custom Properties")]
+        [Description("表示文字")]
+        public string DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                _displayText = value;
+                this.Text = value;
+                Invalidate();  // ボタンの再描画
+            }
         }
     }
 }
