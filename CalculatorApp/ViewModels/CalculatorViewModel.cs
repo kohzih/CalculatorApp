d43c9ec3a4f1e38144a5ae68dc1df33545cdba0d
kohzih <kohzih@gmail.com>
2024-05-19 13:48:07 +0900

    fix: 計算ボタン(=)、演算子ボタンを連続で押した場合に正しく計算されるように修正

--- CalculatorApp/ViewModels/CalculatorViewModel.cs
+++ CalculatorApp/ViewModels/CalculatorViewModel.cs
@@ -13,9 +13,13 @@ namespace CalculatorApp.ViewModels
     internal class CalculatorViewModel : INotifyPropertyChanged
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(CalculatorViewModel));
-        private double _currentValue;
-        private string _displayText;
+        
         private IOperation _currentOperation;
+        private double _currentValue;
+        private double _newValue;
+        private string _displayText;
+        private bool _isNewEntry = true;
+        private bool _isNewOperationButtonPressed = false;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -36,28 +40,54 @@ namespace CalculatorApp.ViewModels
 
         public void EnterNumber(string number)
         {
+            if (!_isNewEntry)
+            {
+                DisplayText = string.Empty;
+                _isNewEntry = true;
+            }
+
             DisplayText += number;
             log.Debug($"Entered number: {number}");
         }
 
         public void SetOperation(IOperation operation, string operationSymbol)
         {
+            if (DisplayText == string.Empty)
+            {
+                return;
+            }
+
+            if (_isNewEntry)
+            {
+                Calculate();
+            }
+
             _currentOperation = operation;
             _currentValue = double.Parse(DisplayText);
-            DisplayText = string.Empty;
+
+            _isNewEntry = false;
+            _isNewOperationButtonPressed = true;
             log.Debug($"Set operation: {operationSymbol} with current value: {_currentValue}");
         }
 
         public void Calculate()
         {
-            if (_currentOperation != null)
+            if (_currentOperation == null)
             {
-                double newValue = double.Parse(DisplayText);
-                double result = _currentOperation.Execute(_currentValue, newValue);
-                log.Debug($"Calculated result: {_currentValue} {_currentOperation.GetType().Name} {newValue} = {result}");
-                DisplayText = (result == (int)result) ? result.ToString() : result.ToString("F5");
-                _currentOperation = null;
+                return;
             }
+
+            if (_isNewEntry || _isNewOperationButtonPressed)
+            {
+                _newValue = double.Parse(DisplayText);
+            }
+
+            double result = _currentOperation.Execute(_currentValue, _newValue);
+            log.Debug($"Calculated result: {_currentValue} {_currentOperation.GetType().Name} {_newValue} = {result}");
+            DisplayText = (result == (int)result) ? result.ToString() : result.ToString("F5");
+            _currentValue = result;
+            _isNewEntry = false;
+            _isNewOperationButtonPressed = false;
         }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
